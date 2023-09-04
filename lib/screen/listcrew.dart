7ac9793ab5501b272fa834df25d19cde17365ae4
akbar <akbar.maulana26061999@gmail.com>
2023-09-04 11:45:44 +0700

    htttprequest

---
+++ lib/screen/listcrew.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ListCrew extends StatelessWidget {
+  const ListCrew({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+
+        ),
+      ),
+    );
+  }
+}
